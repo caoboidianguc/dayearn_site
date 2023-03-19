a07ba7ebcba9a700b05507937d7d405fc543c8ab
caoboidianguc <69663074+caoboidianguc@users.noreply.github.com>
2023-03-18 22:23:34 -0400

    models added

--- ledger/admin.py
+++ ledger/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Technician, Khach
+from .models import Technician, Khach, Service
 
 # Register your models here.
 admin.site.register(Technician)
 admin.site.register(Khach)
+admin.site.register(Service)
--- ledger/models.py
+++ ledger/models.py
@@ -23,9 +23,16 @@ class Khach(models.Model):
     email = models.EmailField(max_length=35, null=True)
     owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.DO_NOTHING)
     technician = models.ForeignKey(Technician, on_delete=models.CASCADE, null=True)
+    diem = models.PositiveIntegerField()
+    ngay = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    desc = models.TextField(max_length=250,blank=True)
     
     
     def __str__(self) -> str:
         return self.name
     
-    
+class Service(models.Model):
+    dichVu = models.CharField(max_length=30)
+    gia = models.FloatField()
+    khach = models.ForeignKey(Khach, on_delete=models.DO_NOTHING, null=True)
