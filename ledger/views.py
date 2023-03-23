98f82292463b5b2226a7abe4527f97d1c61258a3
caoboidianguc <69663074+caoboidianguc@users.noreply.github.com>
2023-03-22 22:21:51 -0400

    add templates

--- ledger/views.py
+++ ledger/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from .models import Technician, Khach, Service
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views import View
 
 # Create your views here.
+
+class AllEmployee(LoginRequiredMixin, View):
+    template = ''
+    def get(self,request):
+        employee = Technician.objects.all()
+        cont = {'employee': employee}
+        return render(request, self.template, cont)
+    
+class AllServices(LoginRequiredMixin, View):
+    template = ''
+    def get(self, request):
+        serv = Service.objects.all()
+        cont = {'dvu': serv}
+        return render(request,self.template,cont)
